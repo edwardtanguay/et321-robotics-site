a0ac92e7223a9d565d7f0077a69fa75d81401160
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-21 15:42:53 +0200

    pages display well

--- src/pages/PageStartseite.tsx
+++ src/pages/PageStartseite.tsx
@@ -1,50 +1,6 @@
 export const PageStartseite = () => {
 	return (
 		<>
-			<p>111 This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
-			<p>This is the Startseite.</p>
 			<p>This is the Startseite.</p>
 		</>
 	);
