58d4badc7b4ffb9bf3152dffc8788aab9ff55c44
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-21 15:04:50 +0200

    all pages finished

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { PageProdukte } from './pages/PageProdukte';
 import { PageTechnologie } from './pages/PageTechnologie';
 import { PageDownloads } from './pages/PageDownloads';
 import { PageUnternehmen } from './pages/PageUnternehmen';
+import { PageLoesungen } from './pages/PageLoesungen';
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 				<NavLink to="/startseite">Startseite</NavLink>
 				<NavLink to="/produkte">Produkte</NavLink>
 				<NavLink to="/technologie">Technologie</NavLink>
+				<NavLink to="/loesungen">Lösungen</NavLink>
 				<NavLink to="/downloads">Downloads</NavLink>
 				<NavLink to="/unternehmen">Unternehmen</NavLink>
 			</nav>
@@ -22,6 +24,7 @@ function App() {
 				<Route path="/startseite" element={<PageStartseite />} />
 				<Route path="/produkte" element={<PageProdukte />} />
 				<Route path="/technologie" element={<PageTechnologie />} />
+				<Route path="/loesungen" element={<PageLoesungen />} />
 				<Route path="/downloads" element={<PageDownloads />} />
 				<Route path="/unternehmen" element={<PageUnternehmen />} />
 				<Route path="/" element={<Navigate to="/startseite" replace />} />
---
+++ src/pages/PageLoesungen.tsx
@@ -0,0 +1,7 @@
+export const PageLoesungen = () => {
+	return (
+		<div className="page pageLoesungen">
+			<p>This is the Lösungen page.</p>
+		</div>
+	);
+};
