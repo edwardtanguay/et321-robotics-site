445eb45e97be4504d44d8d7df12e927fee1a6e37
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-21 15:02:01 +0200

    created Downloads page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 import { PageStartseite } from './pages/PageStartseite';
 import { PageProdukte } from './pages/PageProdukte';
 import { PageTechnologie } from './pages/PageTechnologie';
+import { PageDownloads } from './pages/PageDownloads';
 
 function App() {
 	return (
@@ -12,12 +13,14 @@ function App() {
 				<NavLink to="/startseite">Startseite</NavLink>
 				<NavLink to="/produkte">Produkte</NavLink>
 				<NavLink to="/technologie">Technologie</NavLink>
+				<NavLink to="/downloads">Downloads</NavLink>
 			</nav>
 
 			<Routes>
 				<Route path="/startseite" element={<PageStartseite />} />
 				<Route path="/produkte" element={<PageProdukte />} />
 				<Route path="/technologie" element={<PageTechnologie />} />
+				<Route path="/downloads" element={<PageDownloads />} />
 				<Route path="/" element={<Navigate to="/startseite" replace />} />
 			</Routes>
 		</div>
---
+++ src/pages/PageDownloads.tsx
@@ -0,0 +1,7 @@
+export const PageDownloads = () => {
+	return (
+		<div className="page pageDownloads">
+			<p>This is the Downloads page.</p>
+		</div>
+	);
+};
